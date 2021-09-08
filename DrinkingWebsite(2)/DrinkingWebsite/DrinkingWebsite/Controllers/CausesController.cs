8e0bc9aa5b0a0ad3777371e32bb87655916d21ee
xzhao555 <84710921+xzhao555@users.noreply.github.com>
2021-09-08 12:28:19 +0800

    Iteration 2

---
+++ DrinkingWebsite(2)/DrinkingWebsite/DrinkingWebsite/Controllers/CausesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DrinkingWebsite.Controllers
+{
+    public class CausesController : Controller
+    {
+        // GET: Causes
+        /// <summary>
+        /// cause and risk factor page's controller
+        /// </summary>
+        /// <returns>cause and risk factor view</returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
